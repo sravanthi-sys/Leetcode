f6f92b690459aae91518afa372a5ec688fbb59e2
kanurisravanthi <144417416+sravanthi-sys@users.noreply.github.com>
2025-09-25 11:46:28 +0530

    Time: 0 ms (100%), Space: 12.6 MB (91.62%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,20 +1,11 @@
-class Solution {
-    int solve(int i,int j,vector<vector<int>>& triangle,vector<vector<int>>& memo,int n){
-        if(i==n-1){
-            return triangle[i][j];
-        }
-        if(j> i){
-            return 1e9;
-        }
-        if(memo[i][j] != -1)return memo[i][j];
-        int bottom=solve(i+1,j,triangle,memo,n);
-        int bottomRight=solve(i+1,j+1,triangle,memo,n);
-        return memo[i][j]=min(bottom,bottomRight)+triangle[i][j];
-    }
+class Solution {    
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        int n=triangle.size();
-        vector<vector<int>> memo(n,vector<int>(n,-1));
-        return solve(0,0,triangle,memo,n);
+        for (int i = triangle.size() - 2; i >= 0; i--)
+            for (int j = 0; j < triangle[i].size(); j++)
+                triangle[i][j] +=
+                    min(triangle[i + 1][j], triangle[i + 1][j + 1]);
+
+        return triangle[0][0];
     }
 };
